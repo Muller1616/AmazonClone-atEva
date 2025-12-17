9ed88f7fce1c565e1bce25e27a94bb249e48f205
Muller1616 <mulugetaabeje16@gmail.com>
2025-12-17 16:21:35 +0300

    the category page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './components/Header/Header'
 import Carousel from './components/Carousel/Carousel'
+import Category from './components/Category/Category'
 
 const App = () => {
   return (
     <>
     <Header/>
     <Carousel/>
+    <Category/>
     </>
   )
 }
