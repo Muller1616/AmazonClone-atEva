3a6fa761235f200d02b821e77cb21b81e5dc9b39
Muller1616 <mulugetaabeje16@gmail.com>
2025-12-17 08:47:45 +0300

    carousel page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './components/Header/Header'
+import Carousel from './components/Carousel/Carousel'
 
 const App = () => {
   return (
     <>
     <Header/>
+    <Carousel/>
     </>
   )
 }
