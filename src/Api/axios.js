068fe6212ef2f51b7598c459ae522e9cc2897d1b
Muller1616 <mulugetaabeje16@gmail.com>
2025-12-27 11:03:23 +0300

    deploy the backend at render

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 const axiosInstance = axios.create({
   // baseURL: 'http://127.0.0.1:5001/clone-d68d3/us-central1/api', // The API (cloud function) URL
-  baseURL:'https://api-qlhlims4dq-uc.a.run.app/'
+  // deployed version of function backend
+  // baseURL:'https://api-qlhlims4dq-uc.a.run.app/'
+
+// deployed version of backend render 
+  baseURL: 'https://amazon-api-deploy-28zg.onrender.com/'
 });
 export { axiosInstance };
