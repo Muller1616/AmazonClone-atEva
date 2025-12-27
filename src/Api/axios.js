01a5d54a82a87416445f46e99d97bb1456e6ae71
Muller1616 <mulugetaabeje16@gmail.com>
2025-12-27 10:36:06 +0300

    deploy the backend function

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 const axiosInstance = axios.create({
-  baseURL: 'http://127.0.0.1:5001/clone-d68d3/us-central1/api', // The API (cloud function) URL
+  // baseURL: 'http://127.0.0.1:5001/clone-d68d3/us-central1/api', // The API (cloud function) URL
+  baseURL:'https://api-qlhlims4dq-uc.a.run.app/'
 });
 export { axiosInstance };
